5055874b1b00ef73b1cbf2dc688f767dd1afe6ba
pmp-cs <179293443+pmp-cs@users.noreply.github.com>
2026-04-01 10:15:57 -0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -5,7 +5,6 @@ Create Database and Schemas
 Script Purpose:
     This script creates database 'DataWarehouse' after checking whether 'DataWarehouse' has already existed.
     If it has existed, this script will delete and recreate it.
-    After that, the script also creates three Schemas using Tables within 'DataWarehouse': 'bronze', 'silver' and 'gold'.
 
 WARNING:
     Running this script will delete existing database 'DataWarehouse' permanently.
@@ -18,14 +17,3 @@ DROP DATABASE IF EXISTS DataWarehouse;
 -- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
 USE DataWarehouse;
-
--- Create Schemas by Tables
-CREATE TABLE bronze (
-	id INT
-);
-CREATE TABLE silver (
-	id INT
-);
-CREATE TABLE gold (
-	id INT
-);
